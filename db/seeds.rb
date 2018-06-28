e20e2d6c5ccf76e9db8236060f9fb8fe44c630e0
jrambold <jrambold@gmail.com>
2018-06-28 08:46:13 -0600

    AWS config

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+meals = %w[Breakfast Snacks Lunch Dinner]
+
+meals.each do |meal|
+  Meal.create({name: meal})
+end
